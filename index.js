d1966b227a41b2657664d852f65863bce455dc57
Hanna Hummal <hanna.hummal@khk.ee>
2022-03-25 22:34:00 +0200

    public directory use

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.static('public'));
 
 app.get('/user/:username', (req, res)=> {
     // get parameter data from address row
