987a42df917a63544b34003f5d7b9eaea875e04a
Hanna Hummal <hanna.hummal@khk.ee>
2022-03-25 22:37:28 +0200

    templating directory setup

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.static('public'));
+// add views directory path
+app.set('views', path.join(__dirname, 'views'));
+// add views template engine
+app.set('view engine', 'ejs');
 
 app.get('/user/:username', (req, res)=> {
     // get parameter data from address row
